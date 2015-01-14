74a33a59e371b2702b294969efb315caf6e71ea3
bluelovers <codelovers@users.sourceforge.net>
2015-01-14 23:18:11 +0800

    exec: function(fn)

--- jquery.color.js
+++ jquery.color.js
@@ -569,6 +569,15 @@
 				return 'dark';
 			}
 		},
+
+		exec: function(fn)
+		{
+			var args = Array.prototype.slice.call(arguments, 1) || [];
+
+			var ret = fn.apply(this, args);
+
+			return ret === undefined ? this : ret;
+		},
 	});
 	color.fn.parse.prototype = color.fn;
 
