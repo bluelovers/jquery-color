3c6a381204260097872da5efe50218a0c056adfd
bluelovers <codelovers@users.sourceforge.net>
2015-01-15 02:41:24 +0800

    HSV←→HSL https://gist.github.com/xpansive/1337890

--- jquery.color.js
+++ jquery.color.js
@@ -670,6 +670,37 @@
 		{
 			return color(options || [255, 255, 255, 1]).rand();
 		},
+
+		// https://gist.github.com/xpansive/1337890
+		hsv2hsl: function(hue, sat, val)
+		{
+			return [
+				//[hue, saturation, lightness]
+				//Range should be between 0 - 1
+				hue, //Hue stays the same
+
+				//Saturation is very different between the two color spaces
+				//If (2-sat)*val < 1 set it to sat*val/((2-sat)*val)
+				//Otherwise sat*val/(2-(2-sat)*val)
+				//Conditional is not operating with hue, it is reassigned!
+				sat * val / ((hue = (2 - sat) * val) < 1 ? hue : 2 - hue),
+				hue / 2 //Lightness is (2-sat)*val/2
+				//See reassignment of hue above
+			],
+		},
+
+		hsl2hsv: function(hue, sat, light)
+		{
+			sat *= light < .5 ? light : 1 - light;
+
+			return [
+				//[hue, saturation, value]
+				//Range should be between 0 - 1
+				hue, //Hue stays the same
+				2 * sat / (light + sat), //Saturation
+				light + sat //Value
+			],
+		},
 	});
 
 	// hsla conversions adapted from:
