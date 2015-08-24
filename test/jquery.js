a93e4a1ca07536c4c390a4c326d4178f148ac719
bluelovers <codelovers@users.sourceforge.net>
2015-08-25 07:33:10 +0800

    update (Step: 2)
    Step: 2
    
    [ M] modified - Changes not staged for commit:
    
    	jquery.color.js
    	test/jquery.js
    	test/unit/hsva.js

--- test/jquery.js
+++ test/jquery.js
@@ -1,26 +1,36 @@
-(function() {
+(function()
+{
 
-var parts = document.location.search.slice( 1 ).split( "&" ),
-	length = parts.length,
-	i = 0,
-	current,
-	version,
-	url;
+	var parts = document.location.search.slice(1).split("&"),
+		length = parts.length,
+		i = 0,
+		current,
+		version,
+		url;
 
-for ( ; i < length; i++ ) {
-	current = parts[ i ].split( "=" );
-	if ( current[ 0 ] === "jquery" ) {
-		version = current[ 1 ];
-		break;
+	for (; i < length; i++)
+	{
+		current = parts[i].split("=");
+		if (current[0] === "jquery")
+		{
+			version = current[1];
+			break;
+		}
 	}
-}
 
-if ( version === "git" ) {
-	url = "http://code.jquery.com/jquery-git.js";
-} else {
-	url = "../external/jquery-" + ( version || "1.11.1" ) + "/jquery.js";
-}
+	if (version === "git")
+	{
+		url = "http://code.jquery.com/jquery-git.js";
+	}
+	else if (!version)
+	{
+		url = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+	}
+	else
+	{
+		url = "../external/jquery-" + (version || "1.11.1") + "/jquery.js";
+	}
 
-document.write( "<script src='" + url + "'></script>" );
+	document.write("<script src='" + url + "'></script>");
 
-}() );
+}());
